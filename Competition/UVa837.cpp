0841916d17fbdc0a292d4d34a3827d64679ffe74
cshung <cshung@gmail.com>
2014-09-12 22:32:22 -0700

    Accepted - 837

--- Competition/UVa837.cpp
+++ Competition/UVa837.cpp
@@ -5,36 +5,11 @@
 
 #include "UVa837.h"
 
-/*
-Sample Input
-1
-
-3
-2.0 2.0 9.0 2.0 0.9
-13.5 2.0 4.0 8.5 0.7
-17.0 10.0 7.0 8.5 0.8
-
-Sample Output
-7
--inf 2.000 1.000
-2.000 4.000 0.900
-4.000 7.000 0.630
-7.000 9.000 0.504
-9.000 13.500 0.560
-13.500 17.000 0.800
-17.000 +inf 1.000
- */
-
-/*
- * Keep having wrong answer - why?
- * I have assumed the lines are all on top of the X-axis, it that true?
- */
-
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <stack>
+#include <map>
 #include <stdio.h>
 
 using namespace std;
@@ -49,6 +24,7 @@ struct segment_endpoint
     double x;
     double transparency;
     bool isEnter;
+	int segment_id;
 };
 
 bool compare_segment(segment_endpoint* first, segment_endpoint* second)
@@ -102,7 +78,10 @@ int UVa837()
         getline(cin, s);
         int number_of_flims;
         cin >> number_of_flims;
+		vector<double> segment_transparencies;
+		vector<bool> segment_state;
         vector<segment_endpoint*> endpoints;
+		int segment_counter = 0;
         for (int i = 0; i < number_of_flims; i++)
         {
             string ix1, iy1, ix2, iy2, ir;
@@ -114,9 +93,38 @@ int UVa837()
             x2 = to_double(ix2);
             y2 = to_double(iy2);
             r = to_double(ir);
+
+			if (y1 <= 0 && y2 <= 0 && (y1 + y2 < 0))
+			{
+				// A segment under x-axis can simply be ignored
+				continue;
+			}
+
+			if (y1 * y2 <= 0)
+			{
+				if (y1 != 0 || y2 != 0)
+				{
+					// A segment crossing (or touching) x-axis
+					// (x2-x1)/(y2-y1) = (cx - x1)/(0 - y1)
+					double cx = x1 - y1 * (x2 - x1) / (y2 - y1);
+					if (y1 <= 0)
+					{
+						x1 = cx;
+						y1 = 0;
+					}
+					else if (y2 <= 0)
+					{
+						x2 = cx;
+						y2 = 0;
+					}
+				}
+			}
+
             segment_endpoint* start = new segment_endpoint();
             segment_endpoint* end = new segment_endpoint();
+			start->segment_id = segment_counter;
             start->x = min(x1, x2);
+			end->segment_id = segment_counter;
             end->x = max(x1, x2);
             start->transparency = r;
             end->transparency = r;
@@ -124,75 +132,89 @@ int UVa837()
             end->isEnter = false;
             endpoints.push_back(start);
             endpoints.push_back(end);
+			segment_counter++;
+			segment_transparencies.push_back(r);
+			segment_state.push_back(false);
         }
 
-        sort(endpoints.begin(), endpoints.end(), compare_segment);
+		if (endpoints.size() == 0)
+		{
+			cout << "-inf +inf 1.000";
+		}
+		else
+		{
+			sort(endpoints.begin(), endpoints.end(), compare_segment);
+			vector<pair<double, double> > results;
 
+			for (vector<segment_endpoint*>::iterator p = endpoints.begin(); p != endpoints.end(); p++)
+			{
+				segment_endpoint* endpoint = *p;
+				if (endpoint->isEnter)
+				{
+					segment_state[endpoint->segment_id] = true;
+				}
+				else
+				{
+					segment_state[endpoint->segment_id] = false;
+				}
 
-        stack<double> transparencies;
-        transparencies.push(1.0000000000000000000001);
-        vector<pair<double, double> > results;
-        for (vector<segment_endpoint*>::iterator p = endpoints.begin(); p != endpoints.end(); p++)
-        {
-            segment_endpoint* endpoint = *p;
-            double current_transparency;
-            if (endpoint->isEnter)
-            {
-                current_transparency = transparencies.top() * endpoint->transparency;
-                transparencies.push(current_transparency);
-            }
-            else
-            {
-                transparencies.pop();
-                current_transparency = transparencies.top();
-            }
+				double current_transparency = 1;
+				for (int i = 0; i < segment_transparencies.size(); i++)
+				{
+					if (segment_state[i])
+					{
+						current_transparency *= segment_transparencies[i];
+					}
+				}
 
-            results.push_back(pair<double, double>(endpoint->x, current_transparency));
-            delete endpoint;
-        }
+				results.push_back(pair<double, double>(endpoint->x, current_transparency));
+				delete endpoint;
+			}
 
-        // results is a list of (x, t), where I expect after x, the transparency is t
-        // Now I need t to transform it into segments
-        // consider a walk from -inf to +inf
-        double current_position = negative_infinity;
-        double current_transparency = 1.0;
+			// results is a list of (x, t), where I expect after x, the transparency is t
+			// Now I need t to transform it into segments
+			// consider a walk from -inf to +inf
+			double current_position = negative_infinity;
+			double current_transparency = 1.0;
 
-        int count = results.size();
-        if (results.begin()->first == negative_infinity)
-        {
-            count--;
-        }
-        if (results.rbegin()->first != positive_infinity)
-        {
-            count++;
-        }
-        cout << count << endl;
-        int printed = 0;
-        for (vector<pair<double, double> >::iterator p = results.begin(); p != results.end(); p++)
-        {
-            pair<double, double> event_point = *p;
-            if (p->first > current_position)
-            {
-                cout << to_my_string(current_position) << " " << to_my_string(event_point.first) << " " << to_my_string(current_transparency);
-                current_position = event_point.first;
-                printed++;
-                if (printed != count)
-                {
-                    cout << endl;
-                }
-            }
-            current_transparency = event_point.second;
-        }
-        if (current_position != positive_infinity)
-        {
-            cout << to_my_string(current_position) << " " << to_my_string(positive_infinity) << " " << to_my_string(1.0);
-        }
-        
+			int count = results.size();
+			if (results.begin()->first == negative_infinity)
+			{
+				count--;
+			}
+			if (results.rbegin()->first != positive_infinity)
+			{
+				count++;
+			}
+			cout << count << endl;
+			int printed = 0;
+			for (vector<pair<double, double> >::iterator p = results.begin(); p != results.end(); p++)
+			{
+				pair<double, double> event_point = *p;
+				if (p->first > current_position)
+				{
+					cout << to_my_string(current_position) << " " << to_my_string(event_point.first) << " " << to_my_string(current_transparency);
+					current_position = event_point.first;
+					printed++;
+					if (printed != count)
+					{
+						cout << endl;
+					}
+				}
+				current_transparency = event_point.second;
+			}
+			if (current_position != positive_infinity)
+			{
+				cout << to_my_string(current_position) << " " << to_my_string(positive_infinity) << " " << to_my_string(1.0);
+			}
+		}
         if (c != (number_of_cases - 1))
         {
             cout << endl;
             cout << endl;
         }
     }
+
+	cout << endl;
     return 0;
 }

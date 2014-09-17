776d7cc4f8486840a2c41440ae73393b5e33e15b
andrewau <andrewau@microsoft.com>
2014-09-17 16:58:47 -0700

    Partial code for 127

--- Competition/UVa127.cpp
+++ Competition/UVa127.cpp
@@ -5,11 +5,35 @@
 #include "UVa127.h"
 
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
+struct card
+{
+    char num;
+    char deck;
+};
+
 int UVa127()
 {
+    while (true)
+    {
+        card current_card;
+        cin >> current_card.num;
+        if (current_card.num == '#')
+        {
+            break;
+        }
+        cin >> current_card.deck;
 
+        stack<card> decks;
+
+        for (int i = 0; i < 51; i++)
+        {
+
+        }
+
+    }
     return 0;
 }

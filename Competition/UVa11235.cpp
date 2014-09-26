46787ef370629598d1dd8299e40a19b9fba380f9
andrewau <andrewau@microsoft.com>
2014-09-26 06:31:35 -0700

    UVa Problem 11235 finally accepted

--- Competition/UVa11235.cpp
+++ Competition/UVa11235.cpp
@@ -51,8 +51,12 @@ int process_query(interval_tree_node* node, int query_from, int query_to)
         }
         else
         {
-            int left_answer = process_query(node->left, max(node->left->from, query_from), min(node->left->to, query_to));
-            int right_answer = process_query(node->right, max(node->right->from, query_from), min(node->right->to, query_to));
+            int leftFrom = max(node->left->from, query_from);
+            int leftTo = min(node->left->to, query_to);
+            int rightFrom = max(node->right->from, query_from);
+            int rightTo = min(node->right->to, query_to);
+            int left_answer = leftFrom < leftTo ? process_query(node->left, leftFrom, leftTo) : 0;
+            int right_answer = rightFrom < rightTo ? process_query(node->right, rightFrom, rightTo) : 0;
             return max(left_answer, right_answer);
         }
     }
@@ -60,93 +64,101 @@ int process_query(interval_tree_node* node, int query_from, int query_to)
 
 int UVa11235()
 {
-    int number_of_values, number_of_queries;
-    cin >> number_of_values;
-    cin >> number_of_queries;
-    list<interval_tree_node*> ranges;
-
-    // Step 1: Read the input as ranges
-    bool first = true;
-    int last = -1;
-    int last_index = 0;
-    for (int i = 0; i < number_of_values; i++)
+    while (true)
     {
-        int current_value;
-        cin >> current_value;
-        if (first)
+        int number_of_values, number_of_queries;
+        cin >> number_of_values;
+        if (number_of_values == 0)
         {
-            first = false;
+            break;
         }
-        else
+        cin >> number_of_queries;
+        list<interval_tree_node*> ranges;
+
+        // Step 1: Read the input as ranges
+        bool first = true;
+        int last = -1;
+        int last_index = 0;
+        for (int i = 0; i < number_of_values; i++)
         {
-            if (last != current_value)
+            int current_value;
+            cin >> current_value;
+            if (first)
             {
-                ranges.push_back(new interval_tree_node(last_index, i, (i - last_index)));
-                last_index = i;
-            }
-        }
-        last = current_value;
-    }
-
-    ranges.push_back(new interval_tree_node(last_index, number_of_values, (number_of_values - last_index)));
-
-    // Step 2: Build the tree
-    list<interval_tree_node*> empty_list;
-
-    list<interval_tree_node*>* layer_below = &ranges;
-    list<interval_tree_node*>* layer_above = &empty_list;
-
-    while (layer_below->size() > 1)
-    {
-        interval_tree_node* left = NULL;
-        interval_tree_node* right = NULL;
-        for (list<interval_tree_node*>::iterator ii = layer_below->begin(); ii != layer_below->end(); ii++)
-        {
-            if (left == NULL)
-            {
-                left = *ii;
+                first = false;
             }
             else
             {
-                // Step 2.1: Any two nodes in the bottom layer has a parent node
-                right = *ii;
-                interval_tree_node* parent = new interval_tree_node(left->from, right->to, max(left->answer, right->answer));
-                parent->left = left;
-                parent->right = right;
-                left = right = NULL;
-                layer_above->push_back(parent);
+                if (last != current_value)
+                {
+                    ranges.push_back(new interval_tree_node(last_index, i, (i - last_index)));
+                    last_index = i;
+                }
             }
+            last = current_value;
         }
-        if (left != NULL)
+
+        ranges.push_back(new interval_tree_node(last_index, number_of_values, (number_of_values - last_index)));
+
+        // Step 2: Build the tree
+        list<interval_tree_node*> empty_list;
+
+        list<interval_tree_node*>* layer_below = &ranges;
+        list<interval_tree_node*>* layer_above = &empty_list;
+
+        while (layer_below->size() > 1)
         {
-            // Step 2.2: The orphan last node will be made to the parent layer
-            layer_above->push_back(left);
+            interval_tree_node* left = NULL;
+            interval_tree_node* right = NULL;
+            for (list<interval_tree_node*>::iterator ii = layer_below->begin(); ii != layer_below->end(); ii++)
+            {
+                if (left == NULL)
+                {
+                    left = *ii;
+                }
+                else
+                {
+                    // Step 2.1: Any two nodes in the bottom layer has a parent node
+                    right = *ii;
+                    interval_tree_node* parent = new interval_tree_node(left->from, right->to, max(left->answer, right->answer));
+                    parent->left = left;
+                    parent->right = right;
+                    left = right = NULL;
+                    layer_above->push_back(parent);
+                }
+            }
+            if (left != NULL)
+            {
+                // Step 2.2: The orphan last node will be made to the parent layer
+                layer_above->push_back(left);
+            }
+
+            // Step 2.3: Swap the role of above and below to reuse the list
+            list<interval_tree_node*>* temp = layer_below;
+            layer_below = layer_above;
+            layer_above = temp;
+
+            // Step 2.4: Prepare for the next iteration, above should be empty
+            layer_above->clear();
         }
 
-        // Step 2.3: Swap the role of above and below to reuse the list
-        list<interval_tree_node*>* temp = layer_below;
-        layer_below = layer_above;
-        layer_above = temp;
+        interval_tree_node* root = *(layer_below->begin());
 
-        // Step 2.4: Prepare for the next iteration, above should be empty
-        layer_above->clear();
+        for (int i = 0; i < number_of_queries; i++)
+        {
+            int query_from;
+            int query_to;
+            cin >> query_from;
+            cin >> query_to;
+            /*
+             * query_from - 1 is conversion from 1 based index to 0 based,
+             * query_to is conversion from 1 based index to 0 based, and then become an exclusive index
+             */
+            cout << process_query(root, query_from - 1, query_to) << endl;
+        }
+
+        delete root;
     }
-
-    interval_tree_node* root = *(layer_below->begin());
-
-    for (int i = 0; i < number_of_queries; i++)
-    {
-        int query_from;
-        int query_to;
-        cin >> query_from;
-        cin >> query_to;
-        cout << process_query(root, query_from, query_to) << endl;
-    }
-
-    int zero;
-    cin >> zero; // The meaningless zero input
-
-    delete root;
 
     return 0;
 }

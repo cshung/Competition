ddfa1930660b0e0f40189d8eda63e2203c43d1ed
cshung <cshung@gmail.com>
2015-10-03 11:19:57 -0700

    Find the Duplicate Number fixed

--- Competition/LEET_FIND_THE_DUPLICATE_NUMBER.cpp
+++ Competition/LEET_FIND_THE_DUPLICATE_NUMBER.cpp
@@ -23,27 +23,44 @@ namespace _LEET_FIND_THE_DUPLICATE_NUMBER
             int end = size;
             while (begin < end)
             {
+#ifdef LOG
+                cout << "Answer should be in [" << begin << ", " << end << ")" << endl;
+#endif
                 if ((end - begin) == 1)
                 {
                     return begin;
                 }
                 int mid = (begin + end) / 2;
-                int sum = 0;
+                int lCount = 0;
+                int rCount = 0;
                 for (int i = 0; i < size; i++)
                 {
-                    if (nums[i] < mid)
+                    if (begin <= nums[i] && nums[i] < mid)
                     {
-                        sum += nums[i];
+                        lCount++;
+                    }
+                    else if (mid <= nums[i] && nums[i] < end)
+                    {
+                        rCount++;
                     }
                 }
-                int noDupSum = (mid - 1) * mid / 2;
-                if (sum <= noDupSum)
+#ifdef LOG
+                cout << "Count in [" << begin << ", " << mid << ") is " << lCount << endl;
+                cout << "Count in [" << mid << ", " << end << ") is " << rCount << endl;
+#endif
+                int lCountExpected = mid - begin;
+                int rCountExpected = end - mid;
+                if (lCount > lCountExpected)
+                {
+                    end = mid;
+                }
+                else if (rCount > rCountExpected)
                 {
                     begin = mid;
                 }
                 else
                 {
-                    end = mid;
+                    throw 1;
                 }
             }
         }
@@ -58,8 +75,8 @@ int LEET_FIND_THE_DUPLICATE_NUMBER()
     int case2[] = { 1, 2, 3, 4, 5, 6, 7, 7 };
     int case3[] = { 4, 4, 16, 3, 12, 18, 11, 19, 10, 17, 4, 15, 4, 9, 4, 13, 4, 4, 1, 4 };
     Solution solution;
-    //cout << solution.findDuplicate(vector<int>(case1, case1 + _countof(case1))) << endl;
-    //cout << solution.findDuplicate(vector<int>(case2, case2 + _countof(case2))) << endl;
+    cout << solution.findDuplicate(vector<int>(case1, case1 + _countof(case1))) << endl;
+    cout << solution.findDuplicate(vector<int>(case2, case2 + _countof(case2))) << endl;
     cout << solution.findDuplicate(vector<int>(case3, case3 + _countof(case3))) << endl;
     return 0;
 }

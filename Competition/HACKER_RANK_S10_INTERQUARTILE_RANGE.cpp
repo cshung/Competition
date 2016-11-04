1e3049df4864b90a17e74a0b8dccc8ce067af7da
Andrew Au <cshung@gmail.com>
2016-11-03 23:42:24 -0700

    Template

---
+++ Competition/HACKER_RANK_S10_INTERQUARTILE_RANGE.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+
+// https://www.hackerrank.com/challenges/s10-interquartile-range
+
+#include <iostream>
+using namespace std;
+
+int HACKER_RANK_S10_INTERQUARTILE_RANGE()
+{
+    return 0;
+}

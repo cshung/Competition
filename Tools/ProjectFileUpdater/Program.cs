10f1962ead79a3d6cdf07da01a09d6dd95d1edaf
Andrew Au <cshung@gmail.com>
2016-11-12 12:24:58 -0800

    Completed Project File Updater

--- Tools/ProjectFileUpdater/Program.cs
+++ Tools/ProjectFileUpdater/Program.cs
@@ -20,36 +20,75 @@ namespace ProjectFileUpdater
             codeDirectory = Path.GetDirectoryName(codeDirectory);
             codeDirectory = Path.GetDirectoryName(codeDirectory);
             codeDirectory = Path.Combine(codeDirectory, "Competition");
-            string projectFileName = Path.Combine(codeDirectory, "Competition.vs2013.vcxproj.template");
-            string newProjectFileName = Path.Combine(codeDirectory, "Competition.vs2013.vcxproj");
-            using (var projectFileStream = File.OpenRead(projectFileName))
+
+            foreach (var templateFilePath in Directory.GetFiles(codeDirectory, "*.template"))
             {
-                using (var newProjectFileStream = File.OpenWrite(newProjectFileName))
+                string instanceFileName = Path.GetFileNameWithoutExtension(templateFilePath);
+                string instanceFilePath = Path.Combine(codeDirectory, instanceFileName);
+                using (var projectFileStream = File.OpenRead(templateFilePath))
                 {
-                    XDocument doc = XDocument.Load(projectFileStream);
-                    XElement includeNode = (XElement)doc.DescendantNodes().First(n => n is XElement && ((XElement)n).Name.LocalName.Equals("ClIncludeTemplate"));
-                    includeNode.Name = XName.Get("ItemGroup", includeNode.Name.Namespace.NamespaceName);
-                    foreach (var headerFileName in Directory.GetFiles(codeDirectory, "*.h").OrderBy(fileName => fileName))
+                    using (var newProjectFileStream = File.OpenWrite(instanceFilePath))
                     {
-                        XElement includeElement = new XElement(XName.Get("ClInclude", includeNode.Name.Namespace.NamespaceName));
-                        includeElement.Add(new XAttribute("Include", Path.GetFileName(headerFileName)));
-                        includeNode.Add(includeElement);
-                    }
-                    XElement compileNode = (XElement)doc.DescendantNodes().First(n => n is XElement && ((XElement)n).Name.LocalName.Equals("ClCompileTemplate"));
-                    compileNode.Name = XName.Get("ItemGroup", compileNode.Name.Namespace.NamespaceName);
-                    foreach (var headerFileName in Directory.GetFiles(codeDirectory, "*.cpp").OrderBy(fileName => fileName))
-                    {
-                        if (!string.Equals(Path.GetFileNameWithoutExtension(headerFileName), "stdafx", StringComparison.OrdinalIgnoreCase))
+                        XDocument doc = XDocument.Load(projectFileStream);
+                        XElement includeNode = (XElement)doc.DescendantNodes().FirstOrDefault(n => n is XElement && ((XElement)n).Name.LocalName.Equals("ClIncludeTemplate"));
+                        if (includeNode != null)
                         {
-                            XElement compileElement = new XElement(XName.Get("ClCompile", compileNode.Name.Namespace.NamespaceName));
-                            compileElement.Add(new XAttribute("Include", Path.GetFileName(headerFileName)));
-                            compileNode.Add(compileElement);
+                            includeNode.Name = XName.Get("ItemGroup", includeNode.Name.Namespace.NamespaceName);
+                            foreach (var headerFileName in Directory.GetFiles(codeDirectory, "*.h").OrderBy(fileName => fileName))
+                            {
+                                XElement includeElement = new XElement(XName.Get("ClInclude", includeNode.Name.Namespace.NamespaceName));
+                                includeElement.Add(new XAttribute("Include", Path.GetFileName(headerFileName)));
+                                includeNode.Add(includeElement);
+                            }
                         }
+                        XElement includeFilterNode = (XElement)doc.DescendantNodes().FirstOrDefault(n => n is XElement && ((XElement)n).Name.LocalName.Equals("ClIncludeFilterTemplate"));
+                        if (includeFilterNode != null)
+                        {
+                            includeFilterNode.Name = XName.Get("ItemGroup", includeFilterNode.Name.Namespace.NamespaceName);
+                            foreach (var headerFileName in Directory.GetFiles(codeDirectory, "*.h").OrderBy(fileName => fileName))
+                            {
+                                XElement includeFilterElement = new XElement(XName.Get("ClInclude", includeFilterNode.Name.Namespace.NamespaceName));
+                                includeFilterElement.Add(new XAttribute("Include", Path.GetFileName(headerFileName)));
+                                XElement filterElement = new XElement(XName.Get("Filter", includeFilterNode.Name.Namespace.NamespaceName));
+                                filterElement.Add(new XText("Header Files"));
+                                includeFilterElement.Add(filterElement);
+                                includeFilterNode.Add(includeFilterElement);
+                            }
+                        }
+
+                        XElement compileNode = (XElement)doc.DescendantNodes().FirstOrDefault(n => n is XElement && ((XElement)n).Name.LocalName.Equals("ClCompileTemplate"));
+                        if (compileNode != null)
+                        {
+                            compileNode.Name = XName.Get("ItemGroup", compileNode.Name.Namespace.NamespaceName);
+                            foreach (var headerFileName in Directory.GetFiles(codeDirectory, "*.cpp").OrderBy(fileName => fileName))
+                            {
+                                if (!string.Equals(Path.GetFileNameWithoutExtension(headerFileName), "stdafx", StringComparison.OrdinalIgnoreCase))
+                                {
+                                    XElement compileElement = new XElement(XName.Get("ClCompile", compileNode.Name.Namespace.NamespaceName));
+                                    compileElement.Add(new XAttribute("Include", Path.GetFileName(headerFileName)));
+                                    compileNode.Add(compileElement);
+                                }
+                            }
+                        }
+
+                        XElement compileFilterNode = (XElement)doc.DescendantNodes().FirstOrDefault(n => n is XElement && ((XElement)n).Name.LocalName.Equals("ClCompileFilterTemplate"));
+                        if (compileFilterNode != null)
+                        {
+                            compileFilterNode.Name = XName.Get("ItemGroup", compileFilterNode.Name.Namespace.NamespaceName);
+                            foreach (var headerFileName in Directory.GetFiles(codeDirectory, "*.cpp").OrderBy(fileName => fileName))
+                            {
+                                XElement compileFilterElement = new XElement(XName.Get("ClCompile", compileFilterNode.Name.Namespace.NamespaceName));
+                                compileFilterElement.Add(new XAttribute("Include", Path.GetFileName(headerFileName)));
+                                XElement filterElement = new XElement(XName.Get("Filter", compileFilterNode.Name.Namespace.NamespaceName));
+                                filterElement.Add(new XText("Source Files"));
+                                compileFilterElement.Add(filterElement);
+                                compileFilterNode.Add(compileFilterElement);
+                            }
+                        }
+                        doc.Save(newProjectFileStream);
                     }
-                    doc.Save(newProjectFileStream);
                 }
             }
-
         }
     }
 }

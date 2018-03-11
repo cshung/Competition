1c49f11dc045b3091e0f8d62687347b75cf9a837
Andrew Au <cshung@gmail.com>
2018-03-11 14:12:04 -0700

    todo - h1

---
+++ codechef/src/h1.rs
@@ -0,0 +1,6 @@
+// https://www.codechef.com/problems/H1
+
+pub fn codechef_h1()
+{
+    println!("TODO");
+}
--- codechef/src/main.rs
+++ codechef/src/main.rs
@@ -1,9 +1,11 @@
 // mod nw1;
 // mod git01;
-mod rectangl;
+// mod rectangl;
+mod h1;
 
 fn main() {
     // nw1::codechef_nw1();
     // git01::codechef_git01();
-    rectangl::codechef_rectangl();
+    // rectangl::codechef_rectangl();
+    h1::codechef_h1();
 }

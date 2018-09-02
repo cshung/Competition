01f533f25e89b4c2a0dcf86344b0efaf8dbd0114
Andrew Au <cshung@gmail.com>
2018-09-01 18:15:23 -0700

    Some updates

--- Competition/LEET_MAXIMUM_FREQUENCY_STACK.cpp
+++ Competition/LEET_MAXIMUM_FREQUENCY_STACK.cpp
@@ -14,10 +14,12 @@ using namespace std;
 
 namespace _LEET_MAXIMUM_FREQUENCY_STACK
 {
-    class FreqStack {
+    class FreqStack
+    {
     public:
-        FreqStack() {
-
+        FreqStack()
+        {
+            this->m_top = this->m_bottom = nullptr;
         }
 
         void push(int x)
@@ -35,8 +37,8 @@ namespace _LEET_MAXIMUM_FREQUENCY_STACK
                 newNode->m_stack = m_bottom;
                 newNode->m_val = x;
                 newNode->m_from = nullptr;
-                newNode->m_next = m_bottom->m_m_top;
-                m_bottom->m_m_top = newNode;
+                newNode->m_next = m_bottom->m_top;
+                m_bottom->m_top = newNode;
                 m_valueToNodeMap.insert(make_pair(x, newNode));
             }
             else
@@ -46,7 +48,7 @@ namespace _LEET_MAXIMUM_FREQUENCY_STACK
                 if (myStack->m_next == nullptr)
                 {
                     m_top = new MyStack();
-                    m_top->m_m_top = nullptr;
+                    m_top->m_top = nullptr;
                     m_top->m_prev = myStack;
                     m_top->m_next = nullptr;
                     myStack->m_next = m_top;
@@ -55,8 +57,8 @@ namespace _LEET_MAXIMUM_FREQUENCY_STACK
                 newNode->m_stack = myStack->m_next;
                 newNode->m_val = x;
                 newNode->m_from = oldNode;
-                newNode->m_next = myStack->m_next->m_m_top;
-                myStack->m_next->m_m_top = newNode;
+                newNode->m_next = myStack->m_next->m_top;
+                myStack->m_next->m_top = newNode;
                 m_valueToNodeMap[x] = newNode;
             }
         }
@@ -64,8 +66,8 @@ namespace _LEET_MAXIMUM_FREQUENCY_STACK
         int pop()
         {
             assert(m_top != nullptr);
-            assert(m_top->m_m_top != nullptr);
-            MyStackNode* toRemoveNode = m_top->m_m_top;
+            assert(m_top->m_top != nullptr);
+            MyStackNode* toRemoveNode = m_top->m_top;
             int result = toRemoveNode->m_val;
             if (toRemoveNode->m_from == nullptr)
             {
@@ -76,8 +78,8 @@ namespace _LEET_MAXIMUM_FREQUENCY_STACK
                 m_valueToNodeMap[result] = toRemoveNode->m_from;
             }
 
-            m_top->m_m_top = toRemoveNode->m_next;
-            if (m_top->m_m_top == nullptr)
+            m_top->m_top = toRemoveNode->m_next;
+            if (m_top->m_top == nullptr)
             {
                 MyStack* toRemoveStack = m_top;
                 m_top = toRemoveStack->m_prev;
@@ -101,21 +103,21 @@ namespace _LEET_MAXIMUM_FREQUENCY_STACK
 
         struct MyStackNode
         {
-            MyStack*            m_stack;
-            MyStackNode*		m_from;
-            int			        m_val;
-            MyStackNode*		m_next;
+            MyStack* m_stack;
+            MyStackNode* m_from;
+            int m_val;
+            MyStackNode* m_next;
         };
 
         struct MyStack
         {
-            MyStackNode*	m_m_top;
-            MyStack*        m_prev;
-            MyStack*	    m_next;
+            MyStackNode* m_top;
+            MyStack* m_prev;
+            MyStack* m_next;
         };
 
-        MyStack* m_bottom = nullptr;
-        MyStack* m_top = nullptr;
+        MyStack* m_bottom;
+        MyStack* m_top;
         unordered_map<int, MyStackNode*> m_valueToNodeMap;
     };
 
--- Competition/LEET_SUM_OF_SUBSEQUENCE_WIDTHS.cpp
+++ Competition/LEET_SUM_OF_SUBSEQUENCE_WIDTHS.cpp
@@ -54,7 +54,7 @@ using namespace _LEET_SUM_OF_SUBSEQUENCE_WIDTHS;
 int LEET_SUM_OF_SUBSEQUENCE_WIDTHS()
 {
     Solution solution;
-    int test_array[] = { 5,69,89,92,31,16,25,45,63,40,16,56,24,40,75,82,40,12,50,62,92,44,67,38,92,22,91,24,26,21,100,42,23,56,64,43,95,76,84,79,89,4,16,94,16,77,92,9,30,13 };
+    int test_array[] = { 2, 1, 3 };
     vector<int> test(test_array, test_array + _countof(test_array));
     cout << solution.sumSubseqWidths(test) << endl;
     return 0;

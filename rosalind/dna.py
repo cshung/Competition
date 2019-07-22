e167204e90fb4b48f18a7ff9a52c243d3e9b02d2
Andrew Au <cshung@gmail.com>
2019-07-21 19:03:21 -0700

    Rosalind - Counting DNA Nucleotides

---
+++ rosalind/dna.py
@@ -0,0 +1,17 @@
+# http://rosalind.info/problems/dna/
+
+dna = raw_input()
+a = 0
+c = 0
+g = 0
+t = 0
+for n in dna:
+	if n == 'A':
+		a = a + 1
+	elif n == 'C':
+		c = c + 1
+	elif n == 'G':
+		g = g + 1
+	elif n == 'T':
+		t = t + 1
+print("%d %d %d %d" % (a,c,g,t))

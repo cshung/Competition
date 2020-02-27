4a8670c7991abbf6e16ef8935ac93b511dc04f97
Andrew Au <cshung@gmail.com>
2020-02-26 22:46:00 -0800

    Improve Shortest Word Distance II

--- Competition/LEET_SHORTEST_WORD_DISTANCE_II.cpp
+++ Competition/LEET_SHORTEST_WORD_DISTANCE_II.cpp
@@ -8,6 +8,7 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -18,62 +19,75 @@ namespace _LEET_SHORTEST_WORD_DISTANCE_II
     public:
         WordDistance(vector<string>& words)
         {
-            for (auto word : words)
+            for (int i = 0; i < words.size(); i++)
             {
-                auto probe = word_to_id.find(word);
-                int word_id;
-                if (probe == word_to_id.end())
+                auto probe = word_to_positions.find(words[i]);
+                if (probe == word_to_positions.end())
                 {
-                    word_id = word_to_id.size();
-                    word_to_id.insert(make_pair(word, word_id));
+                    vector<int> list;
+                    list.push_back(i);
+                    word_to_positions.insert(make_pair(words[i], list));
                 }
                 else
                 {
-                    word_id = probe->second;
+                    probe->second.push_back(i);
                 }
-                word_ids.push_back(word_id);
             }
         }
 
         int shortest(string word1, string word2)
         {
-            int word_id_1 = word_to_id[word1];
-            int word_id_2 = word_to_id[word2];
-            int last1 = -1;
-            int last2 = -1;
             int min_dist = -1;
-            for (int i = 0; i < word_ids.size(); i++)
+            int p1 = 0;
+            int p2 = 0;
+            int last_1 = -1;
+            int last_2 = -1;
+            vector<int>& list_1 = word_to_positions[word1];
+            vector<int>& list_2 = word_to_positions[word2];
+            while (true)
             {
-                if (word_ids[i] == word_id_1)
+                int curr_1 = -1;
+                int curr_2 = -1;
+                if (p1 == list_1.size() && p2 == list_2.size())
                 {
-                    last1 = i;
-                    if (last2 != -1)
+                    break;
+                }
+                else if (p1 == list_1.size())
+                {
+                    curr_2 = list_2[p2++];
+                }
+                else if (p2 == list_2.size())
+                {
+                    curr_1 = list_1[p1++];
+                }
+                else
+                {
+                    if (list_1[p1] < list_2[p2])
                     {
-                        int dist = last1 - last2;
-                        if (min_dist == -1 || dist < min_dist)
-                        {
-                            min_dist = dist;
-                        }
+                        curr_1 = list_1[p1++];
+                    }
+                    else
+                    {
+                        curr_2 = list_2[p2++];
                     }
                 }
-                else if (word_ids[i] == word_id_2)
+                if (last_1 != -1 && curr_2 != 1)
                 {
-                    last2 = i;
-                    if (last1 != -1)
-                    {
-                        int dist = last2 - last1;
-                        if (min_dist == -1 || dist < min_dist)
-                        {
-                            min_dist = dist;
-                        }
-                    }
+                    int dist = curr_2 - last_1;
+                    min_dist = (min_dist == -1) ? dist : min(min_dist, dist);
                 }
+                else if (last_2 != -1 && curr_1 != -1)
+                {
+                    int dist = curr_1 - last_2;
+                    min_dist = (min_dist == -1) ? dist : min(min_dist, dist);
+                }
+                last_1 = curr_1;
+                last_2 = curr_2;
             }
             return min_dist;
         }
     private:
-        unordered_map<string, int> word_to_id;
-        vector<int> word_ids;
+        unordered_map<string, vector<int>> word_to_positions;
     };
 
 };

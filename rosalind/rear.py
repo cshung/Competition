fa6596c03691b1470c95add2aaa3b86545451ec5
Andrew Au <cshung@gmail.com>
2020-08-23 17:05:52 -0700

    Rosalind - Reversal Distance

--- rosalind/rear.py
+++ rosalind/rear.py
@@ -2,90 +2,80 @@
 
 from sys import stdin
 from collections import deque
-
-def num(l, i):
-  if i == len(l):
-    return 0
-  else:
-    return l[i] + 10 * num(l, i + 1)
+from math import factorial
+import datetime
 
 def prepare():
+  print (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+  last_progress = 0
   n = 10
-  graph = {}
-  prepare_worker(list(range(0, n)), 0, graph)
-  return graph
-
-last_progress = 0
-
-def prepare_worker(l, i, graph):
-  n = len(l)
-  if i == n:
-    neighbors = []
-    graph[num(l, 0)]=neighbors
-    global last_progress
-    progress = len(graph) * 100 // 3628800
-    if progress != last_progress:
-      print(progress)
-      last_progress = progress
+  done = factorial(n)
+  root = list(range(1, n + 1))
+  bfs_perm = deque()
+  bfs_dist = deque()
+  distances = {}
+  bfs_perm.append(root)
+  bfs_dist.append(0)
+  distances[str(root)] = 0
+  while len(bfs_perm) > 0:
+    perm = bfs_perm.popleft()
+    dist = bfs_dist.popleft()
     for i in range(0, n):
       for j in range(i + 1, n):
         left = i
         right = j 
         while left < right:
-          temp = l[left]
-          l[left] = l[right]
-          l[right] = temp
+          temp = perm[left]
+          perm[left] = perm[right]
+          perm[right] = temp
           left = left + 1
           right = right - 1
-        neighbors.append(num(l, 0))
+        key = str(perm)
+        if key not in distances:
+          bfs_perm.append(list(perm))
+          bfs_dist.append(dist + 1)
+          distances[key] = dist + 1
+          progress = len(distances) * 100 // done
+          if progress != last_progress:
+            print(progress)
+            last_progress = progress
+          if (len(distances) == done):
+            break
         left = i
         right = j 
         while left < right:
-          temp = l[left]
-          l[left] = l[right]
-          l[right] = temp
+          temp = perm[left]
+          perm[left] = perm[right]
+          perm[right] = temp
           left = left + 1
           right = right - 1
-  else:
-    for j in range(i, len(l)):
-      temp = l[i]
-      l[i] = l[j]
-      l[j] = temp
-      prepare_worker(l, i + 1, graph)
-      temp = l[i]
-      l[i] = l[j]
-      l[j] = temp
+  print (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+  return distances
 
-def reversal_distance(source, target, graph):
-  if source == target:
-    return 0
-  source_number = num([int(i) - 1 for i in source.split(" ")], 0)
-  target_number = num([int(i) - 1 for i in target.split(" ")], 0)
-  queue = deque()
-  enqueued = {}
-  queue.append((0, source_number))
-  enqueued[source_number] = True
-  while len(queue) > 0:
-    (distance, visiting) = queue.popleft()
-    for neighbor in graph[visiting]:
-      if visiting == target_number:
-        return distance + 1
-      else:
-        queue.append((distance + 1, neighbor))
+def reversal_distance(source, target, answers):
+  map = {}
+  for canon in range(0, len(source)):
+    map[source[canon]] = canon + 1
+  mapped_target = []
+  for v in target:
+    mapped_target.append(map[v])
+  return answers[str(mapped_target)]
 
-def rear(data):
+def rear(data, answers):
   n = len(data) // 2
   for i in range(0, n):
-    print(reversal_distance(data[2 * i], data[2 * i + 1], graph))
+    print(reversal_distance(data[2 * i], data[2 * i + 1], answers))
 
 def main():
-  graph = prepare()
+  answers = prepare()
   data = []
   for line in stdin:
     line = line.strip()
     if line != '':
-      data.append(line)
-  print(rear(data))
+      data.append([int(token) for token in line.split()])
+      if len(data) == 2:
+        print(reversal_distance(data[0], data[1], answers))
+        data = []
 
 if __name__ == "__main__":
   main()

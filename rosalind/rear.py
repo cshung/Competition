d5f4de98d888e88823c68ed989c2dcd56f934e74
Andrew Au <andrewau@Andrews-MacBook-Pro.local>
2020-08-27 20:21:32 -0700

    wip

--- rosalind/rear.py
+++ rosalind/rear.py
@@ -6,56 +6,49 @@ from math import factorial
 import datetime
 
 def encode(perm):
-    code = list(perm)
-    for i in range(0, len(code)):
-        count = 0
-        for j in range(i + 1, len(code)):
-            if code[i] > code[j]:
-                count = count + 1
-            else:
-                code[j] = code[j] - 1
-    return code
+  code = list(perm)
+  for i in range(0, len(code)):
+    count = 0
+    for j in range(i + 1, len(code)):
+      if code[i] > code[j]:
+        count = count + 1
+      else:
+        code[j] = code[j] - 1
+  answer = 0
+  for i in range(0, len(code)):
+    answer = answer + factorial(i) * code[len(code) - i - 1]
+  return answer
 
-def decode(perm):
-    code = list(perm)
-    for k in range(0, len(code)):
-        i = len(code) - k - 1
-        count = 0
-        for j in range(i + 1, len(code)):
-            if code[i] > code[j]:
-                count = count + 1
-            else:
-                code[j] = code[j] + 1
-    return code    
-
-def to_number(code):
-    answer = 0
-    for i in range(0, len(code)):
-        answer = answer + factorial(i) * code[len(code) - i - 1]
-    return answer
-
-def from_number(n, length):
-    code = [0] * length
-    for i in range(0, length):
-        remainder = n % (i + 1)
-        code[len(code) - 1 - i] = remainder
-        n = n // (i + 1)
-    return code
+def decode(n, length):
+  perm = [0] * length
+  for i in range(0, length):
+    remainder = n % (i + 1)
+    perm[length - 1 - i] = remainder
+    n = n // (i + 1)
+  for k in range(0, len(perm)):
+    i = len(perm) - k - 1
+    count = 0
+    for j in range(i + 1, len(perm)):
+      if perm[i] > perm[j]:
+        count = count + 1
+      else:
+        perm[j] = perm[j] + 1
+  return perm
 
 def prepare():
   print (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
   last_progress = 0
-  n = 10
+  n = 5
   done = factorial(n)
-  root = list(range(1, n + 1))
+  root = list(range(0, n))
   bfs_perm = deque()
   bfs_dist = deque()
   distances = {}
-  bfs_perm.append(root)
+  bfs_perm.append(encode(root))
   bfs_dist.append(0)
-  distances[str(root)] = 0
+  distances[encode(root)] = 0
   while len(bfs_perm) > 0:
-    perm = bfs_perm.popleft()
+    perm = decode(bfs_perm.popleft(), n)
     dist = bfs_dist.popleft()
     for i in range(0, n):
       for j in range(i + 1, n):
@@ -67,9 +60,9 @@ def prepare():
           perm[right] = temp
           left = left + 1
           right = right - 1
-        key = str(perm)
+        key = encode(perm)
         if key not in distances:
-          bfs_perm.append(list(perm))
+          bfs_perm.append(key)
           bfs_dist.append(dist + 1)
           distances[key] = dist + 1
           progress = len(distances) * 100 // done
@@ -92,11 +85,11 @@ def prepare():
 def reversal_distance(source, target, answers):
   map = {}
   for canon in range(0, len(source)):
-    map[source[canon]] = canon + 1
+    map[source[canon] - 1] = canon
   mapped_target = []
   for v in target:
-    mapped_target.append(map[v])
-  return answers[str(mapped_target)]
+    mapped_target.append(map[v - 1])
+  return answers[encode(mapped_target)]
 
 def rear(data, answers):
   n = len(data) // 2

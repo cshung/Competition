d4f6391286cddeb02ee68413559dde3bdeafc0cc
Andrew Au <cshung@gmail.com>
2020-12-06 17:35:10 -0800

    LeetCode challenge checking tool

---
+++ Tools/Check/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                line = line.Trim();
+                if (!line.StartsWith("Week"))
+                {
+                    sb.AppendLine(line.Replace("+10 LeetCoin", Environment.NewLine));
+                }
+            }
+            List<string> left = new List<string>();
+            List<bool> right = new List<bool>();
+            foreach (var problem in sb.ToString().Split("\n", StringSplitOptions.RemoveEmptyEntries).Select(t => t.Trim()))
+            {
+                if (problem.Length > 0)
+                {
+                    string filename = string.Format(@"c:\dev\Competition\Competition\LEET_{0}.cpp", problem.ToUpper().Replace(" ", "_"));
+                    bool exists = File.Exists(filename);
+                    left.Add(problem);
+                    right.Add(exists);
+                }
+            }
+            int max = left.Select(t => t.Length).Max();
+            for (int i = 0; i < left.Count; i++)
+            {
+                Console.WriteLine(left[i] + new string(' ', max + 2 - left[i].Length) + right[i]);
+            }
+        }
+    }
+}
---
+++ Tools/Check/README.md
@@ -0,0 +1,4 @@
+# README
+The tool checks if a LeetCode challenge problem is completed.
+
+Copy and paste the table from the webpage into a text file, and then pipe the file into the tool to check if the problem is completed.

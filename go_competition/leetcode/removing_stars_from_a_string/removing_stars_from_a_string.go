0ae2a81adf9ca9beb1e50ee474bdc40a359307f4
Andrew Au <cshung@gmail.com>
2023-04-10 19:50:38 -0700

    Removing Stars From a String

---
+++ go_competition/leetcode/removing_stars_from_a_string/removing_stars_from_a_string.go
@@ -0,0 +1,26 @@
+package removing_stars_from_a_string
+
+import (
+	"fmt"
+	"strings"
+)
+
+func removeStars(s string) string {
+	var stack []rune
+	builder := strings.Builder{}
+	for _, char := range s {
+		if char == '*' {
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, char)
+		}
+	}
+	for i := 0; i < len(stack); i++ {
+		builder.WriteString(string(stack[i]))
+	}
+	return builder.String()
+}
+
+func Removing_stars_from_a_string() {
+	fmt.Println(removeStars("leet**cod*e"))
+}
--- go_competition/main.go
+++ go_competition/main.go
@@ -1,11 +1,18 @@
 package main
 
-// import "main/leetcode/construct_quad_tree"
-// import "main/leetcode/linked_list_cycle_ii"
+import "main/leetcode/construct_quad_tree"
+import "main/leetcode/linked_list_cycle_ii"
 import "main/leetcode/clone_graph"
+import "main/leetcode/removing_stars_from_a_string"
 
 func main() {
-	// construct_quad_tree.Construct_quad_tree()
-	// linked_list_cycle_ii.Linked_list_cycle_ii()
-	clone_graph.Clone_graph()
+	if 1 == 0 {
+		//
+		// Please the Go compiler happy with dead code
+		//
+		construct_quad_tree.Construct_quad_tree()
+		linked_list_cycle_ii.Linked_list_cycle_ii()
+		clone_graph.Clone_graph()
+	}
+	removing_stars_from_a_string.Removing_stars_from_a_string()
 }

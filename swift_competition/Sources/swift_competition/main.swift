60251e9c03270a4917c78f08861f11251c36a658
Andrew Au <cshung@gmail.com>
2023-07-06 22:45:25 -0700

    Maximize the Confusion of an Exam

--- swift_competition/Sources/swift_competition/main.swift
+++ swift_competition/Sources/swift_competition/main.swift
@@ -1,3 +1,10 @@
-// single_number_ii.main()
-// longest_subarray_of_1s_after_deleting_one_element.main()
-minimum_size_subarray_sum.main()
+func one() -> Int {
+    return 1
+}
+
+if 1 != one() {
+  single_number_ii.main()
+  longest_subarray_of_1s_after_deleting_one_element.main()
+  minimum_size_subarray_sum.main()
+}
+maximize_the_confusion_of_an_exam.main()

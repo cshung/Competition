ffc5a6e14916d91b57dcbe8135867acaa43365bd
Andrew Au <cshung@gmail.com>
2023-07-07 19:55:53 -0700

    Put Marbles in Bags

--- swift_competition/Sources/swift_competition/main.swift
+++ swift_competition/Sources/swift_competition/main.swift
@@ -6,5 +6,6 @@ if 1 != one() {
   single_number_ii.main()
   longest_subarray_of_1s_after_deleting_one_element.main()
   minimum_size_subarray_sum.main()
+  maximize_the_confusion_of_an_exam.main()
 }
-maximize_the_confusion_of_an_exam.main()
+put_marbles_in_bags.main()

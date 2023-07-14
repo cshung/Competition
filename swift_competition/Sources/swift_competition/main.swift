4e12f8204be6ab831f93c3274892eb7a1577639d
Andrew Au <cshung@gmail.com>
2023-07-14 10:23:50 -0700

    Longest Arithmetic Subsequence of Given Difference

--- swift_competition/Sources/swift_competition/main.swift
+++ swift_competition/Sources/swift_competition/main.swift
@@ -12,5 +12,6 @@ if 1 != one() {
   minimum_depth_of_binary_tree.main()
   all_nodes_distance_k_in_binary_tree.main()
   find_eventual_safe_states.main()
+  course_schedule.main()
 }
-course_schedule.main()
+longest_arithmetic_subsequence_of_given_difference.main()

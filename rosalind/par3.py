8df8f06185787c0f7ed261585dc7de8471603056
Andrew Au <cshung@gmail.com>
2024-02-15 09:02:21 -0800

    Rosalind - 3-Way Partition

---
+++ rosalind/par3.py
@@ -0,0 +1,60 @@
+# http://rosalind.info/problems/par/
+
+from sys import stdin
+
+def par(array):
+  n = len(array)
+  p = 0
+  q = 1
+  r = n - 1
+  pivot = array[0]
+  while True:
+    #
+    # Loop invariants
+    # array[0, p) < pivot
+    # array[p, q) = pivot
+    # array(r, n) > pivot
+    #
+    while r > q and array[r] > pivot:
+      r -= 1
+    #
+    # At this point, unless r == q, otherwise array[r] <= pivot
+    #
+    while q < r and array[q] <= pivot:
+      if array[q] < pivot:
+        array[p], array[q] = array[q], array[p]
+        p += 1
+      q += 1
+    #
+    # At this point, unless r == q, otherwise array[q] > pivot
+    #
+    if r == q:
+      #
+      # The loop invariant does not include what happen with array[q]
+      # fix that here, the p, q adjustment is useful only if we plan
+      # to check the loop invariants outside the loop for testing only.
+      #
+      if array[q] < pivot:
+        array[p], array[q] = array[q], array[p]
+        p += 1
+        q += 1
+      elif array[q] == pivot:
+        q += 1
+      break
+    #
+    # Note that we do not adjust q here, that saves us from determining
+    # whether array[q] == pivot or not, the invariants stay correct that way
+    #
+    array[r], array[q] = array[q], array[r]
+    r -= 1
+  return array
+
+def main():
+  data = []
+  for line in stdin:
+    data.append(line.strip())
+  answers = par([int(x) for x in data[1].split()])
+  print(" ".join([str(x) for x in answers]))
+
+if __name__ == "__main__":
+  main()
